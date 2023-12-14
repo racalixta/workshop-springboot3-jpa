c671c3fe13712aa1bce68cbbf0fb5a85ff3c6d1c
racalixta <ra.calixta@gmail.com>
2023-12-13 21:05:07 -0300

    Service layer, component registration

---
+++ src/main/java/com/racalixta/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.racalixta.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.racalixta.course.entities.User;
+import com.racalixta.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
