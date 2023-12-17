2610886372d629211d1165428f9448b832770e9a
racalixta <ra.calixta@gmail.com>
2023-12-16 21:25:52 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/racalixta/course/config/TestConfig.java
+++ src/main/java/com/racalixta/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.racalixta.course.entities.Category;
 import com.racalixta.course.entities.Order;
+import com.racalixta.course.entities.OrderItem;
 import com.racalixta.course.entities.Product;
 import com.racalixta.course.entities.User;
 import com.racalixta.course.entities.enums.OrderStatus;
 import com.racalixta.course.repositories.CategoryRepository;
+import com.racalixta.course.repositories.OrderItemRepository;
 import com.racalixta.course.repositories.OrderRepository;
 import com.racalixta.course.repositories.ProductRepository;
 import com.racalixta.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +73,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 
