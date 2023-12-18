3ed9aaf26fa5816aa9df2b91f22c0ef13388a656
racalixta <ra.calixta@gmail.com>
2023-12-17 22:23:43 -0300

    Exception handling - findById

--- src/main/java/com/racalixta/course/services/UserService.java
+++ src/main/java/com/racalixta/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.racalixta.course.entities.User;
 import com.racalixta.course.repositories.UserRepository;
+import com.racalixta.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
